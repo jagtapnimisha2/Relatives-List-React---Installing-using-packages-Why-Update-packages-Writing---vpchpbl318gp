3b18ecfe3fd84580fef0d5c5bd2ce660482d641c
jagtapnimisha2 <120504401+jagtapnimisha2@users.noreply.github.com>
2022-12-29 13:59:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,25 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-    let relatives=["r1","r2","r3","r4"];
-        let relativeList=relatives.map((relative))=><h2>{relative}</h2>);
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-                <ol key="relativeList">
-                    <li key="relativeListItem1">{key=relativeListItem1[0]}</li>
-                     <li key="relativeListItem2">{key=relativeListItem1[1]}</li>
-                     <li key="relativeListItem3">{key=relativeListItem1[2]}</li>
-                     <li key="relativeListItem4">{key=relativeListItem1[3]}</li>
-                </ol>
-            </div>
-        );
-    }
-}
+  render() {
+    const relatives = ["r1", "r2", "r3", "r4"];
+    const relativeList = relatives.map((relative) => <h2>{relative}</h2>);
 
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        {
+          <ol key="relativeList">
+            <li key="relativeListItem1">{relatives[0]}</li>
+            <li key="relativeListItem2">{relatives[1]}</li>
+            <li key="relativeListItem3">{relatives[2]}</li>
+            <li key="relativeListItem4">{relatives[3]}</li>
+          </ol>
+        }
+      </div>
+    );
+  }
+}
 
 export default App;
